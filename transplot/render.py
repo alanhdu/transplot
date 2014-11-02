d9620f5da20c0492663a103b48aa2399f6d4bbf9
Alan Du <alanhdu@gmail.com>
2014-11-02 01:35:47 -0400

    Scale SVG correctly

--- transplot/render.py
+++ transplot/render.py
@@ -2,23 +2,25 @@ import pandas as pd
 
 import plot
 
+
+def _scale(pos):
+    return 1000 * (pos - pos.min()) / (pos.max() - pos.min())
+
 def renderSVG(graph, fname="test.svg"):
     import svgwrite
     dwg = svgwrite.Drawing(fname)
-
+    dwg.viewbox(-50, -50, 1100, 1100)
+    dwg.fit("right", "bottom", "meet")
 
     for glyph in graph.glyphs:
         pos = graph.pos if glyph.pos is None else glyph.pos
         color = graph.color if glyph.color is None else glyph.color
 
+        scaled = _scale(pos)
         if isinstance(glyph, plot.Point):
-            scale = 400.0 / (pos.max() - pos.min())
-            pos = (pos - pos.min()) * scale
-
             p1, p2 = pos.columns
-            for x, y in zip(pos[p1], pos[p2]):
-                dwg.add(dwg.circle( (x,y)))
-
+            for x, y in zip(scaled[p1], scaled[p2]):
+                dwg.add(dwg.circle( (x,y), r=10))
     dwg.save()
     return dwg
 
