91154c203698da84a9942050b244a5b5af233f8a
Alan Du <alanhdu@gmail.com>
2014-11-02 21:21:39 -0500

    Add group color

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import transplot as tp
 df = pd.read_csv("/home/alan/workspace/vind/test/iris.csv")
 
 g = tp.plot.Graph(pos=df[["SepalLength", "SepalWidth"]]) \
-        + tp.plot.Points(size=df["PetalLength"])
+        + tp.plot.Points(size=df["PetalLength"], color=tp.plot.Group(df["Name"]))
         #+ tp.plot.Points()
 
 tp.render.renderSVG(g)
--- transplot/render.py
+++ transplot/render.py
@@ -5,6 +5,7 @@ izip = itertools.izip
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 import plot
 
@@ -23,6 +24,19 @@ def _scaleArea(pos, min=8):
     else:
         return np.sqrt(pos / pos.min()) * min
 
+def _rgb2Hex(r, g, b):
+    return "#{:02x}{:02x}{:02x}".format(int(255*r), int(255*g), int(255*b))
+def _groupPalette(group):
+    unique = group.data.unique()
+    palette = sns.color_palette("husl", len(unique))
+    p = {group : _rgb2Hex(r, g, b)
+         for group, (r, g, b) in izip(unique, palette)}
+    for group in group.data:
+        yield p[group]
+
+def _groupColormap(data):
+    pass
+
 def renderSVG(graph, fname="test.svg"):
     import svgwrite
     dwg = svgwrite.Drawing(fname)
@@ -31,20 +45,32 @@ def renderSVG(graph, fname="test.svg"):
 
     for glyph in graph.glyphs:
         pos = graph.pos if glyph.pos is None else glyph.pos
-        color = graph.color if glyph.color is None else glyph.color
         size = graph.size if glyph.size is None else glyph.size
 
         if isinstance(glyph, plot.Points):
             p1, p2 = pos.columns
             scaled = _scaleLinear(pos)
-            pos = izip(scaled[p1], scaled[p1])
+            pos = izip(scaled[p1], scaled[p2])
 
             if not isinstance(size, collections.Iterable):
                 size = itertools.repeat(size)
             else:
                 size = _scaleArea(size)
-            for x, y, r in izip(scaled[p1], scaled[p2], size):
-                dwg.add(dwg.circle( (x,y), r=r))
+
+            if glyph.color is None:
+                if graph.color is None:
+                    color = "blue"
+                elif isinstance(graph.color, plot.Group):
+                    color = _groupPalette(graph.color)
+                else:
+                    color = _groupColormap(graph.color)
+            elif isinstance(glyph.color, plot.Group):
+                color = _groupPalette(glyph.color)
+            else:
+                color = _groupColormap(glyph.color)
+
+            for p, r, c in izip(pos, size, color):
+                dwg.add(dwg.circle(p, r=r, fill=c))
     dwg.save()
     return dwg
 
